c6c39a24afcec4551cfc96ef1da8be5c3b11fa09
Judson <judson.play1@gmail.com>
2020-05-25 19:01:47 -0300

    first commit

---
+++ lib/index.d.ts
@@ -0,0 +1,32 @@
+export declare type GeolocationOptionsProps = {
+    suppressOnMount?: boolean;
+    positionOptions?: {
+        enableHighAccuracy: boolean;
+        timeout: number;
+        maximumAge: number;
+    };
+    watchMode?: boolean;
+};
+export declare type CoordinatesProps = {
+    coords: {
+        latitude: number | null;
+        longitude: number | null;
+        altitude: number | null;
+        accuracy: number | null;
+        altitudeAccuracy: number | null;
+        heading: number | null;
+        speed: number | null;
+    };
+};
+export declare type GeolocationProps = {
+    coords: CoordinatesProps["coords"] | undefined;
+    isEnabled?: boolean;
+    isExpired?: boolean;
+    isAvailable?: boolean;
+    isSupressed?: boolean;
+    invokeRequest: (bool: boolean) => void;
+    watchId: number | undefined;
+};
+declare const _default: ({ suppressOnMount, positionOptions, watchMode, }?: GeolocationOptionsProps) => GeolocationProps;
+export default _default;
+//# sourceMappingURL=index.d.ts.map
